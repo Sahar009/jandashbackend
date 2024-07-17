d49357f62d5e6c5eeb4d21f603ff3abcb9824b9d
Aletile Stanley Babatunde <166507719+PappyBoyisa@users.noreply.github.com>
2024-07-17 03:01:22 -0700

    multer image upload and cloudinary installation only

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { getAllStudents, updateStudentProfile, createNewStudent, deleteStudentProfile } = require('../controllers/StudentController')
+const { upload } = require('../utils/fileUpload')
 
 const router = express.Router()
 
 
 router.get('/getAllStudent',getAllStudents)
-router.post('/createStudentProfile',createNewStudent)
-router.patch('/updateStudent/:id',updateStudentProfile)
+router.post('/createStudentProfile',upload.single('file'),createNewStudent)
+
+router.patch('/updateStudent/:id',upload.single(),updateStudentProfile)
 router.delete('/deleteStudent/:id',deleteStudentProfile)
 
 module.exports = router
